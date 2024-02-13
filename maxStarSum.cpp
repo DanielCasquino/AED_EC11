222f5706c05a1d006e33b6d2a783854ec5830c5e
DanielCasquino <daniel.casquino@utec.edu.pe>
2024-02-13 18:23:54 -0500

    2 done

--- maxStarSum.cpp
+++ maxStarSum.cpp
@@ -1,44 +1,72 @@
-#include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <algorithm>
+
 using namespace std;
-class Solution {
+
+class Solution
+{
 public:
-    int maxStarSum(vector<int>& vals, vector<vector<int>>& edges, int k) {
-        int n = vals.size();
-        vector<vector<int>> adj(n);
-        for (auto& e : edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+    int maxStarSum(vector<int> &vals, vector<vector<int>> &edges, int k)
+    {
+
+        if (edges.size() < 1)
+        {
+            return vals[0];
         }
-        vector<int> subtree(n, 0);
-        vector<int> dp(n, 0);
-        function<void(int, int)> dfs = [&](int u, int p) {
-            subtree[u] = vals[u];
-            for (auto v : adj[u]) {
-                if (v == p) continue;
-                dfs(v, u);
-                subtree[u] += max(0, subtree[v]);
+
+        vector<vector<int>> graph(vals.size());
+
+        for (auto pair : edges)
+        {
+            graph[pair[0]].push_back(pair[1]);
+            graph[pair[1]].push_back(pair[0]);
+        }
+
+        vector<bool> visited(vals.size(), false);
+        queue<int> q;
+
+        int maxSum = 0;
+
+        visited[0] = true;
+        q.push(0);
+
+        while (!q.empty())
+        {
+            int current = q.front();
+            q.pop();
+            int tempSum = vals[current];
+
+            vector<int> starValues;
+
+            for (int neighbour : graph[current])
+            {
+                if (!visited[neighbour])
+                {
+                    visited[neighbour] = true;
+                    q.push(neighbour);
+                }
+                starValues.push_back(vals[neighbour]);
             }
-        };
-        function<void(int, int)> dfs2 = [&](int u, int p) {
-            dp[u] = subtree[u];
-            for (auto v : adj[u]) {
-                if (v == p) continue;
-                dfs2(v, u);
-                dp[u] = max(dp[u], subtree[u] - max(0, subtree[v]) + dp[v]);
-            }
-        };
-        dfs(0, -1);
-        dfs2(0, -1);
-        return dp[0];
+
+            sort(starValues.begin(), starValues.end(), greater<int>());
+
+            for (int i = 0; i < min((int)graph[current].size(), k); ++i)
+                tempSum += starValues[i];
+
+            if (tempSum > maxSum)
+                maxSum = tempSum;
+        }
+        return maxSum;
     }
 };
-int main() {
-    vector<int> vals = {1,2,3,4,10,-10,-20};
-    vector<vector<int>> edges = {{0,1},{1,2},{1,3},{3,4},{3,5},{3,6}};
+
+int main()
+{
+    vector<int> vals = {1, 2, 3, 4, 10, -10, -20};
+    vector<vector<int>> edges = {{0, 1}, {1, 2}, {1, 3}, {3, 4}, {3, 5}, {3, 6}};
     int k = 2;
-    Solution sol;
-    cout << sol.maxStarSum(vals, edges, k) << endl;
+    cout << Solution().maxStarSum(vals, edges, k) << endl;
     return 0;
 }
